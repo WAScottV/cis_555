648b9250935fd36a19ef6e186ea23b2852a2235a
Alex Scott <williamascott@cedarville.edu>
2018-11-21 11:06:36 -0800

    new method for generating combonations

--- nov_25/brute_force_pdp.js
+++ nov_25/brute_force_pdp.js
@@ -20,7 +20,21 @@ const bruteForcePdp = (L, n) => {
     return results;
 }
 
+// 
 
+const generateCombos = (n, k) => {
+    let results = [];
+    for (let i = 0; i < Math.pow(2, n); i++) {
+        results.push(i.toString(2).padStart(n, '0').split(''));
+    }
+    results = results.map(r => r.map(m => parseInt(m)));
+    // console.log(results);
+    
+    console.log(results.filter(r => r.reduce((a, b) => a + b) === k));
+    
+};
+
+// n! / k! * (n - k)!
 
 const getSequencesForStartPoint = (arr, start, count) => {
     if (count === 1) return arr.map(v => [v]);
@@ -41,5 +55,7 @@ const getSequencesForStartPoint = (arr, start, count) => {
 // const testData = [1, 1, 1, 2, 2, 3, 3, 3, 4, 4, 5, 5, 6, 6, 6, 9, 9, 10, 11, 12, 15];
 // console.log(bruteForcePdp(testData, 7));
 
-const testData = [1, 1, 2, 2, 2, 3, 3, 4, 4, 5, 5, 5, 6, 7, 7, 7, 8, 9, 10, 11, 12];
-console.log(bruteForcePdp(testData, 7));
+// const testData = [1, 1, 2, 2, 2, 3, 3, 4, 4, 5, 5, 5, 6, 7, 7, 7, 8, 9, 10, 11, 12];
+// console.log(bruteForcePdp(testData, 7));
+
+generateCombos(12, 5);
