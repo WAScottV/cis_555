2a7ea228e7a161bb41609dc3a1fdfe3004e20087
Alex Scott <williamascott@cedarville.edu>
2018-12-07 07:34:30 -0500

    progress update

---
+++ dec_8/edit_dist.js
@@ -0,0 +1,14 @@
+const editDistance = (s1, s2) => {
+    const arr = [];
+    arr[0] = [0,0,0,0];
+    arr[1] = [1,1,1,1];
+    console.log(arr);
+    for (let i = 0; i < s1.length; i++) {
+
+        for (let j = 0; j < s2.length; j++) {
+
+        }
+    }
+};
+
+editDistance('', '');
