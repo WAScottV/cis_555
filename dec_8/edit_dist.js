f2f0a29f5ce2d948a521b76a19392eb62cb9ea85
Alex Scott <williamascott@cedarville.edu>
2018-12-08 10:02:10 -0500

    progress update

--- dec_8/edit_dist.js
+++ dec_8/edit_dist.js
@@ -1,14 +1,51 @@
-const editDistance = (s1, s2) => {
-    const arr = [];
-    arr[0] = [0,0,0,0];
-    arr[1] = [1,1,1,1];
-    console.log(arr);
-    for (let i = 0; i < s1.length; i++) {
+const MATCH = '/';
+const INSERT = '|';
+const DELETE = '-';
 
-        for (let j = 0; j < s2.length; j++) {
+const editDistance = (v, w) => {
+    const n = v.length;
+    const m = w.length;
 
+    // init array grid
+    const s = new Array(n + 1);
+    const b = new Array(n + 1);
+    for (let i = 0; i <= n; i++) {
+        s[i] = new Array(m + 1);
+        b[i] = new Array(m + 1);
+    }
+    for (let i = 0; i <= n; i++) {
+        s[i][0] = 0;
+    }
+    for (let j = 1; j <= m; j++) {
+        s[0][j] = 0;
+    }
+    // end init array grid
+
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= m; j++) {
+            let iMinusOne = s[i - 1][j];
+            let jMinusOne = s[i][j - 1];
+            let i_j_MinusOne = 0;
+            if (v[i] === w[j]) {
+                i_j_MinusOne = s[i - 1][j - 1] + 1;
+            }
+            s[i][j] = Math.max(iMinusOne, jMinusOne, i_j_MinusOne);
+            
+            switch (s[i][j]) {
+                case s[i - 1][j]:
+                    b[i][j] = INSERT;
+                    break;
+                case s[i][j - 1]:
+                    b[i][j] = DELETE;
+                    break;
+                case s[i - 1][j - 1] + 1:
+                    b[i][j] = MATCH;    
+                    break;
+            }
         }
     }
+    console.log(s);
+    console.log(b);
 };
 
-editDistance('', '');
+editDistance('atcgtac', 'atgttat');
