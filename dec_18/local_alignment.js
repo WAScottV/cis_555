3de00f631657b9c85bb3322a8bfdb7ad7c3bf402
Alex Scott <williamascott@cedarville.edu>
2018-12-12 21:12:33 -0500

    update

--- dec_18/local_alignment.js
+++ dec_18/local_alignment.js
@@ -1,19 +1,47 @@
-const location = {
-    v_loc: 0,
-    w_loc: 0
-}
+const traceBack = (d) => {
+    let stringOne = '';
+    let stringTwo = '';
+
+    let next = d.find(arr => arr.find(v => v.val === overallMaxVal)).find(x => x.val === overallMaxVal);
+    
+    while (1) {
+        
+    }
+    console.log(next);
+};
 
 const generateScoringMatrix = (v, w, d) => {
-    const grid = [];
 
-    for (let i = 1; i < v.length; i++) {
-        for (let j = 1; j < w.length; j++) {
+    for (let i = 1; i <= v.length; i++) {
+        for (let j = 1; j <= w.length; j++) {
             const del = d[i - 1][j];
             const ins = d[i][j - 1];
-            const match = d[i - 1][j - 1];
+            const diag = d[i - 1][j - 1];
+            const diagVal = v[i - 1] === w[j - 1] ? diag.val + 1 : diag.val - 1;
+            const x = v[i - 1];
+            const y = w[j - 1];
+            const max = Math.max(del.val - 0.5, ins.val - 0.5, diagVal, 0);
+            d[i][j] = { val: max, prev: [] };
+
+            if (max > overallMaxVal) {
+                overallMaxVal = max;
+            }
+
+            if (del.val - 0.5 === max) {
+                d[i][j].prev.push(del);
+                d[i][j].op = 'del';
+            }
+            if (ins.val - 0.5 === max) {
+                d[i][j].prev.push(ins);
+                d[i][j].op = 'ins';
+            }
+            if (diagVal === max) {
+                d[i][j].prev.push(diag);
+                d[i][j].op = 'match';
+            }
         }
-        const thisOne = { val: 0, prev: [] };
     }
+    return d;
 };
 
 const initGrid = (n, m) => {
@@ -22,12 +50,18 @@ const initGrid = (n, m) => {
         grid[i] = new Array(m + 1);
     }
     for (let i = 0; i <= n; i++) {
-        grid[i][0] = { val: 0, prev: [] };
+        grid[i][0] = { val: 0, prev: [], op: '' };
     }
     for (let j = 1; j <= m; j++) {
-        grid[0][j] = { val: 0, prev: [] };
+        grid[0][j] = { val: 0, prev: [], op: '' };
     }
     return grid;
 }
 
-console.log(initGrid(8, 8));
+const first = '1213434222';
+const second = '1343422421';
+let overallMaxVal = 0;
+
+const d = initGrid(first.length, second.length);
+generateScoringMatrix(first, second, d);
+traceBack(d);
